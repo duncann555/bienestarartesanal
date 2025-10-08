0dde6410b7ca25988b947a0d23534acf99c76ba8
SebastianF <sebaflomen@gmail.com>
2025-10-08 06:27:12 -0300

    se corrigio error en Footer.css a footer.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import "../styles/Footer.css";
+import "../styles/footer.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function Footer() {
